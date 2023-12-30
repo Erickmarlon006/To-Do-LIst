75dbca40e6d2d2382c08e79ba90b50b881c487b2
Erickmarlon006 <erickmarlonborbadesenna@gmail.com>
2023-12-30 14:12:26 -0300

    Componente inicial do site

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Container from './components/Container'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <Container />
+    </>
+
+  )
+}
+
+export default App
